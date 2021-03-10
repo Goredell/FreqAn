7de90c485f401998dccd6e0f489c4e364f3f4b7e
Артём Антощук <Xa4yM6eK@gmail.com>
2021-03-10 18:23:30 +0300

    parallel work on stream

--- FreqAn/Program.cs
+++ FreqAn/Program.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Threading;
+using System.Linq;
+using System.Text;
 
 namespace FreqAn
 {
@@ -12,14 +14,14 @@ namespace FreqAn
 	{
 		static void Main(string[] args)
 		{
+			Stopwatch SW = new Stopwatch();
 			ConcurrentDictionary<string, int> triplets = new ConcurrentDictionary<string, int>();
-			Stack<string> file = new Stack<string>();
 
 
 
 			//TODO better path
 			//Get path
-			string path = "save.txt";
+			string path = @"save.txt";
 			//Console.WriteLine("Default file is " + path + ". Press enter to continue or set new path. \nPath:") ;
 			//string temp = Console.ReadLine();
 			//if(temp != "")
@@ -31,22 +33,10 @@ namespace FreqAn
 			//}
 
 
-			Stopwatch SW = new Stopwatch();
 			SW.Start();
 
-			//TODO parallel reader
-			//Reader
-			using (StreamReader reader = new StreamReader(path))
-			{
-				string readText;
-				while ((readText = reader.ReadLine()) != null)
-					file.Push(readText);
-			}
-
-			Console.WriteLine(	"\nЧтение файла окончено " + SW.Elapsed + 
-								"\nКол-во строчек в файле " + file.Count);
-
-			Parallel.ForEach(file, str =>
+			//Tripplet counter using dictionary
+			Parallel.ForEach(File.ReadLines(@"save.txt"), str =>
 			{
 				Queue<char> word = new Queue<char>();
 				foreach (char c in str)
@@ -56,31 +46,24 @@ namespace FreqAn
 						word.Clear();
 						continue;
 					}
+					word.Enqueue(c);
 					if (word.Count == 3)
 					{
 						string sword = new string(word.ToArray());
 						triplets.AddOrUpdate(sword, 1, (word, oldValue) => oldValue + 1);
 						word.Dequeue();
 					}
-					word.Enqueue(c);
 				}
 			});
 
 			var ans = triplets.ToArray();
 			Array.Sort(ans, (x, y) => y.Value.CompareTo(x.Value));
 			for (int i = 0; i < ans.Length && i < 10; i++)
-				Console.Write(ans[i].ToString() + ", ");
+				Console.Write(ans[i].ToString() + (i < ans.Length-1 && i < 9 ? ", " : "."));
+
 			SW.Stop();
-			Console.WriteLine("\n" + SW.Elapsed);
+			Console.WriteLine("\n" + SW.Elapsed.TotalMilliseconds);
 			Console.ReadKey();
 		}
-		//static Task<Stack<string>> readit(string path)
-		//{
-		//	Stack<string> file = new Stack<string>();
-		//	using (StreamReader reader = File.ReadLines(path)
-		//			.AsParallel().WithDegreeOfParallelism(10)
-		//			ForAll(x => file.Push(x)));
-		//	return file;
-		//}
 	}
 }
